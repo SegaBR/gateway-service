c65fd7507e5ec784db5fa33d76fd9bed3dc35800
Christofer Sega <40266546+SegaBR@users.noreply.github.com>
2022-03-14 23:03:56 -0300

    app

--- index.js
+++ index.js
@@ -60,6 +60,8 @@ function verificaJWT(request, response, next){
     });
 }
 
+app.use(logger('dev'));
+
 app
     .route("/login")
     .post(login) 
@@ -82,10 +84,9 @@ app.all('/alunos/:id', verificaJWT, (req, res, next) => {
     alunosServiceProxy(req, res, next);
 })
 
-app.use(logger('dev'));
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.listen(process.env.PORT || 3000);
+var server = http.createServer(app);
+server.listen(process.env.PORT || 3000);
