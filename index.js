117716c0daead0ded11bde3f01b1f738000adfc8
Christofer Sega <40266546+SegaBR@users.noreply.github.com>
2022-03-14 23:17:18 -0300

    sasasa

--- index.js
+++ index.js
@@ -85,8 +85,6 @@ app.all('/alunos/:id', verificaJWT, (req, res, next) => {
 app.use(logger('dev'));
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-var server = http.createServer(app);
-server.listen(process.env.PORT || 3000);
+app.listen(process.env.PORT || 3000);
